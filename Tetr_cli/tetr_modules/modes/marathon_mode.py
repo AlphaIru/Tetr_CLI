a5f6a15e4d1d3436b57e4b57900e32e85e1cf0a4
Airun_Iru <latioslatias2002@gmail.com>
2025-10-10 01:50:01 -0700

    refactored draw in hold and queue, started on board drawing

--- Tetr_cli/tetr_modules/modes/marathon_mode.py
+++ Tetr_cli/tetr_modules/modes/marathon_mode.py
@@ -8,7 +8,6 @@ from random import shuffle, seed, randint
 
 # from math import floor
 
-
 # TODO: Flowchart:
 # DONE: Generation 0phase
 # Falling phase
@@ -47,11 +46,28 @@ POINTS_TABLE: dict[str, int] = {
     "Tspin_Double": 1200,
     "Tspin_Triple": 1600,
 }
+
+MIN_X: int = 80
+MIN_Y: int = 24
+
 BOARD_WIDTH: int = 10
-DRAW_BOARD_WIDTH: int = BOARD_WIDTH * 2  # Each cell is 2 chars wide
 BOARD_HEIGHT: int = 20
+DRAW_BOARD_WIDTH: int = BOARD_WIDTH * 2  # Each cell is 2 chars wide
 TARGET_FPS: int = 30
 MINO_TYPES: set[str] = {"I", "O", "T", "S", "Z", "J", "L"}
+MINO_COLOR: dict[str, int] = {"O": 1, "I": 2, "T": 3, "L": 4, "J": 5, "S": 6, "Z": 7}
+MINO_DRAW_DATA: dict[str, tuple[list[str], int]] = {
+    "O": (["████    ", "████    "], 1),
+    "I": (["████████", "        "], 2),
+    "T": (["██████  ", "  ██    "], 3),
+    "L": (["    ██  ", "██████  "], 4),
+    "J": (["██      ", "██████  "], 5),
+    "S": (["  ████  ", "████    "], 6),
+    "Z": (["████    ", "  ████  "], 7)
+}
+
+# Mino Draw Location: 
+MINO_DRAW_LOCATION: dict[str, dict[str, tuple[tuple[int, int], tuple[int, int]]]] = {
 
 
 class ModeClass:
@@ -66,16 +82,20 @@ class ModeClass:
         self.__sound_action: dict[str, list[str]] = {"BGM": ["stop"], "SFX": []}
         self.__mode: str = "countdown"
 
-        self.__board = [([0] * BOARD_WIDTH) for _ in range(BOARD_HEIGHT)]
+        self.__board: list[list[int]] = [
+            ([0] * BOARD_WIDTH) for _ in range(BOARD_HEIGHT)
+        ]
         self.__max_y: int = 0
         self.__max_x: int = 0
         self.__offset_x: int = 0
         self.__offset_y: int = 0
 
         self.__current_mino: str = ""
-        self.__current_rotation: str = "0"
+        self.__current_rotation: str = "S"
         self.__current_position: tuple[int, int] = (0, 0)  # (y, x)
         self.__current_hold: str = ""
+        self.__softdrop_cooldown: int = 0
+        self.__lock_delay: int = 0
 
         self.__mino_list: list[str] = []
         self.mino_generation(initial=True)
@@ -92,12 +112,20 @@ class ModeClass:
         self.__sound_action["SFX"] = []
         return sound_action
 
+    def draw_mino(self, stdscr: window, mino: str, y_loc: int, x_loc: int) -> None:
+        """Draw a mino at the given position."""
+        if mino in MINO_DRAW_DATA:
+            lines, color = MINO_DRAW_DATA[mino]
+            for line_num, line in enumerate(lines):
+                stdscr.addstr(y_loc + line_num, x_loc, line, color_pair(color))
+
     def draw_hold(self, stdscr: window) -> window:
         """Draw the Hold Box."""
         horizontal_length: int = 12
         vertical_length: int = 4
         hold_offset_x: int = self.__offset_x - horizontal_length - 3
         horizontal_line: str = "+" + "-" * horizontal_length
+
         stdscr.addstr(
             self.__offset_y - 1,
             hold_offset_x,
@@ -129,6 +157,11 @@ class ModeClass:
             horizontal_line,
             A_BOLD,
         )
+
+        if self.__current_hold:
+            self.draw_mino(
+                stdscr, self.__current_hold, self.__offset_y + 2, hold_offset_x + 2
+            )
         return stdscr
 
     def draw_queue(self, stdscr: window) -> window:
@@ -176,92 +209,12 @@ class ModeClass:
         # O, I, T, L, J, S, Z
         for queue_counter in range(5):
             if len(self.__mino_list) > queue_counter:
-                if self.__mino_list[queue_counter] == "O":
-                    stdscr.addstr(
-                        self.__offset_y + 2 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "  ████  ",
-                        color_pair(1),
-                    )
-                    stdscr.addstr(
-                        self.__offset_y + 3 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "  ████  ",
-                        color_pair(1),
-                    )
-                elif self.__mino_list[queue_counter] == "I":
-                    stdscr.addstr(
-                        self.__offset_y + 2 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "████████",
-                        color_pair(2),
-                    )
-                elif self.__mino_list[queue_counter] == "T":
-                    stdscr.addstr(
-                        self.__offset_y + 2 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        " ██████ ",
-                        color_pair(3),
-                    )
-                    stdscr.addstr(
-                        self.__offset_y + 3 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "   ██   ",
-                        color_pair(3),
-                    )
-                elif self.__mino_list[queue_counter] == "L":
-                    stdscr.addstr(
-                        self.__offset_y + 2 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "    ██",
-                        color_pair(4),
-                    )
-                    stdscr.addstr(
-                        self.__offset_y + 3 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "██████",
-                        color_pair(4),
-                    )
-                elif self.__mino_list[queue_counter] == "J":
-                    stdscr.addstr(
-                        self.__offset_y + 2 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "██",
-                        color_pair(5),
-                    )
-                    stdscr.addstr(
-                        self.__offset_y + 3 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "██████",
-                        color_pair(5),
-                    )
-                elif self.__mino_list[queue_counter] == "S":
-                    stdscr.addstr(
-                        self.__offset_y + 2 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "  ████",
-                        color_pair(6),
-                    )
-                    stdscr.addstr(
-                        self.__offset_y + 3 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "████",
-                        color_pair(6),
-                    )
-                elif self.__mino_list[queue_counter] == "Z":
-                    stdscr.addstr(
-                        self.__offset_y + 2 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "████",
-                        color_pair(7),
-                    )
-                    stdscr.addstr(
-                        self.__offset_y + 3 + queue_counter * 3,
-                        queue_offset_x + 2,
-                        "  ████",
-                        color_pair(7),
-                    )
-
+                self.draw_mino(
+                    stdscr,
+                    self.__mino_list[queue_counter],
+                    self.__offset_y + 2 + queue_counter * 3,
+                    queue_offset_x + 2,
+                )
         return stdscr
 
     def draw_board(self, stdscr: window) -> window:
@@ -272,22 +225,20 @@ class ModeClass:
             self.__offset_y - 1, self.__offset_x - 1, "+" + "-" * DRAW_BOARD_WIDTH + "+"
         )
 
-        # Draw side borders and cells
-        for column, row in enumerate(self.__board):
+        # Draw side borders
+        for row in range(BOARD_HEIGHT):
             stdscr.addstr(
-                self.__offset_y + column, self.__offset_x - 2, "|", A_BOLD
-            )  # Left border
-            for x, cell in enumerate(row):
-                char = "█" if cell else " "
-                stdscr.addstr(
-                    self.__offset_y + column, self.__offset_x + x * 2, char * 2
-                )
+                self.__offset_y + row,
+                self.__offset_x - 1,
+                "|",
+                A_BOLD,
+            )
             stdscr.addstr(
-                self.__offset_y + column,
+                self.__offset_y + row,
                 self.__offset_x + DRAW_BOARD_WIDTH,
                 "|",
                 A_BOLD,
-            )  # Right border
+            )
 
         # Draw bottom border
         stdscr.addstr(
@@ -297,6 +248,32 @@ class ModeClass:
             A_BOLD,
         )
 
+        return stdscr
+
+    def draw_mino_on_board(self, stdscr: window) -> window:
+        """This will draw the current mino on the board."""
+
+        # 1: calculate the current mino's blocks positions
+        # 2: calculate the offset and rotation
+        # 3: calculate its ghost position
+        # 4: add that to the list of blocks to draw
+        # 5: draw the blocks
+
+        draw_board: list[list[int]] = deepcopy(self.__board)
+
+        draw_board[self.__current_position[0]][self.__current_position[1]] = MINO_COLOR[
+            self.__current_mino
+        ]
+
+        for y, row in enumerate(draw_board):
+            for x, cell in enumerate(row):
+                char = "█" if cell else " "
+                stdscr.addstr(
+                    self.__offset_y + y,
+                    self.__offset_x + x * 2,
+                    char * 2,
+                    color_pair(cell) if cell else A_BOLD,  # A_BOLD for empty
+                )
         return stdscr
 
     def countdown_mode(self, stdscr: window) -> None:
@@ -315,10 +292,10 @@ class ModeClass:
 
         self.__countdown -= 1
         if self.__countdown <= 0:
-            self.__mode = "play"
-            self.__sound_action["BGM"] = ["Korobeiniki"]
+            self.__mode = "play_music_wait"
             self.__sound_action["SFX"].append("go")
             stdscr.addstr(self.__max_y // 2, self.__max_x // 2, "Go", A_BOLD)
+            self.__countdown = TARGET_FPS // 2
 
     def mino_generation(self, initial: bool = False) -> None:
         """This will handle the mino generation."""
@@ -337,16 +314,24 @@ class ModeClass:
         """This will handle the play mode."""
         if len(self.__mino_list) <= 14:
             self.mino_generation()
+        if not self.__current_mino:
+            self.__current_mino = self.__mino_list.pop(0)
+            self.__current_rotation = "0"
+            self.__current_position = (0, BOARD_WIDTH // 2 - 1)
 
     def increment_frame(self, stdscr: window, pressed_keys: set[str]) -> window:
         """This will progress the game based on the inputs."""
 
         check_max_y, check_max_x = stdscr.getmaxyx()
+
         if check_max_y != self.__max_y or check_max_x != self.__max_x:
             self.__max_y = check_max_y
             self.__max_x = check_max_x
             self.__offset_y = (check_max_y - BOARD_HEIGHT) // 2
             self.__offset_x = (check_max_x - DRAW_BOARD_WIDTH) // 2
+
+        if check_max_x < MIN_X or check_max_y < MIN_Y:
+            return stdscr
 
         stdscr = self.draw_board(stdscr)
         stdscr = self.draw_queue(stdscr)
@@ -356,10 +341,24 @@ class ModeClass:
             self.__action = "Go_Back"
             return stdscr
 
-        if self.__mode == "play":
-            self.play_mode(stdscr)
-        elif self.__mode == "countdown":
+        if self.__mode == "countdown":
             self.countdown_mode(stdscr)
+            return stdscr
+
+        self.play_mode(stdscr)
+
+        if self.__mode == "play_music_wait":
+            self.__countdown -= 1
+            if self.__countdown <= 0:
+                self.__mode = "play"
+                self.__sound_action["BGM"] = ["Korobeiniki"]
+                return stdscr
+            stdscr.addstr(
+                self.__max_y // 2,
+                self.__max_x // 2,
+                "Go",
+                A_BOLD,
+            )
 
         return stdscr
 
