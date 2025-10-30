caf8b5dc35426bdf80d076ee10be8e92f87a26be
Airun_Iru <latioslatias2002@gmail.com>
2025-10-30 09:48:21 -0700

    linter being an ass

--- tetr_cli/tetr_modules/modes/options/gameplay_option_mode.py
+++ tetr_cli/tetr_modules/modes/options/gameplay_option_mode.py
@@ -1,11 +1,11 @@
 """This is the gameplay option mode."""
 # coding: utf-8
 
-from typing import Dict, List, Set
+# from typing import Dict, List, Set
 
-from curses import window
+# from curses import window
 
-from tetr_cli.tetr_modules.menu_core.menu_mode import TwoDimmMenuModeClass
+# from tetr_cli.tetr_modules.menu_core.menu_mode import TwoDimmMenuModeClass
 
 
 OPTION_LIST: List[str] = [
