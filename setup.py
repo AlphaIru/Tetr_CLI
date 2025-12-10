8e4089dbff551756ca78a69dda849270f60b5b86
Airun_Iru <latioslatias2002@gmail.com>
2025-12-10 10:03:56 -0800

    1.0.3-1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tetr_cli",
-    version="1.0.3",
+    version="1.0.3-1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
